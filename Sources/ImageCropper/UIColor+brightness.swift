0499044a7e1a0527c42bb2390647d708b0667c22
David Giovannini <dsjove@mac.com>
2025-07-12 21:52:08 -0500

    better resize function

--- Sources/ImageCropper/UIColor+brightness.swift
+++ Sources/ImageCropper/UIColor+brightness.swift
@@ -13,10 +13,24 @@ public extension UIColor {
 }
 
 public extension UIImage {
-	func resized(_ targetSize: CGSize) -> UIImage {
-		let renderer = UIGraphicsImageRenderer(size: targetSize)
+	func shrinkTo(_ targetSize: CGSize) -> UIImage {
+		if self.size.width <= targetSize.width && self.size.height <= targetSize.height {
+			return self
+		}
+
+		let widthRatio = targetSize.width / size.width
+		let heightRatio = targetSize.height / size.height
+		let scaleFactor = min(widthRatio, heightRatio)
+
+		// Compute scaled size that fits inside targetSize
+		let scaledSize = CGSize(
+			width: size.width * scaleFactor,
+			height: size.height * scaleFactor
+		)
+
+		let renderer = UIGraphicsImageRenderer(size: scaledSize)
 		return renderer.image { _ in
-			self.draw(in: CGRect(origin: .zero, size: targetSize))
+			self.draw(in: CGRect(origin: .zero, size: scaledSize))
 		}
 	}
 }
